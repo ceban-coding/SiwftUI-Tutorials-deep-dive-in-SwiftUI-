d5006672bfad8d7988344d6d8d0fb0d125e4ebdb
Ion Ceban <ceban.ion1992@gmail.com>
2021-06-29 11:16:22 -0500

    Initial Commit

---
+++ SiwftUI Tutorials/SiwftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiwftUI_TutorialsApp.swift
+//  SiwftUI Tutorials
+//
+//  Created by Ion Ceban on 6/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SiwftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
