0106e28c2ff6eaccabbf2e98839bb6ef61fb0d78
Ion Ceban <ceban.ion1992@gmail.com>
2021-06-30 15:17:26 -0500

    Adde new category and profile	View , now we can see our category in stack scroll view, and profile view .

--- SiwftUI Tutorials/Views/Landmarks/LandmarkDetail.swift
+++ SiwftUI Tutorials/Views/Landmarks/LandmarkDetail.swift
@@ -22,22 +22,17 @@ struct LandmarkDetail: View {
                 .frame(height: 300)
             
             CircleImage(image: landmark.image)
-                .offset(y: -200)
-                .padding(.bottom, -200)
+                .offset(y: -130)
+                .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                HStack {
-                    Text(landmark.name)
-                        .font(.title)
-                        .foregroundColor(.primary)
-                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
-                }
+                Text(landmark.name)
+                    .font(.title)
+
                 HStack {
                     Text(landmark.park)
-                        .font(.subheadline)
                     Spacer()
                     Text(landmark.state)
-                        .font(.subheadline)
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
@@ -49,9 +44,10 @@ struct LandmarkDetail: View {
                 Text(landmark.description)
             }
             .padding()
-            
-            Spacer()
+    
         }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
@@ -60,6 +56,5 @@ struct LandmarkDetail_Previews: PreviewProvider {
 
         static var previews: some View {
             LandmarkDetail(landmark: modelData.landmarks[0])
-                .environmentObject(modelData)
         }
 }
